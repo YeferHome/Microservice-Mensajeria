af050a7b8d5bc7d6c891d42789438a78a9f3435e
yeferHome <yefersonrojas16yi@gmai.com>
2025-04-03 10:15:19 -0500

    Micro Servicio de Mensajeria con dependencias

---
+++ MicroMensajeria/src/main/java/retoPragma/MicroMensajeria/MicroMensajeriaApplication.java
@@ -0,0 +1,13 @@
+package retoPragma.MicroMensajeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroMensajeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroMensajeriaApplication.class, args);
+	}
+
+}
---
+++ MicroMensajeria/src/test/java/retoPragma/MicroMensajeria/MicroMensajeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package retoPragma.MicroMensajeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroMensajeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
